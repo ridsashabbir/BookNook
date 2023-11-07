c1affbd991364193621a58f43097b10a3f3103eb
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-07 23:39:03 +0500

    create first http route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { PORT } from "./config.js";
+
 const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("it is running now");
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to Port: ${PORT}`);
