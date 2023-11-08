00284ee09b681728a9d1454c39c9c17200696fa9
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-08 18:14:48 +0500

    delete a book in database with mongoose

--- backend/index.js
+++ backend/index.js
@@ -91,6 +91,24 @@ app.put("/books/:id", async (request, response) => {
   }
 });
 
+// route to delete a book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    } else {
+      return response
+        .status(200)
+        .json({ message: "Book deleted successfully!" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
