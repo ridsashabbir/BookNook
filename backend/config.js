9b57242e5d5d19de219c988afacab3572a621bde
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-08 13:41:13 +0500

    save a new book with mongoose(create0

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://booknook.kupodxs.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb+srv://ridsashabbir:ridsashabbir@booknook.kupodxs.mongodb.net/books-collection?retryWrites=true&w=majority";
+// export const mongoDBURL =
+//   "mongodb+srv://booknook.kupodxs.mongodb.net/books-collection?retryWrites=true&w=majority";
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -19,4 +19,5 @@ const bookSchema = mongoose.Schema(
 );
 
 // we use export keyword so that we can use this model in another file
-export const Book = mongoose.model("Book", { name: String });
+// export const Book = mongoose.model("Book", { name: String });
+export const Book = mongoose.model("Book", bookSchema);
