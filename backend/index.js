de391e73eb8e6973b2ef23af76c4162abca9a248
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-08 17:52:44 +0500

    get one book by id  with mongoose

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,18 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// route to get one book from database by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
