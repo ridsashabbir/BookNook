6251e9c5f933f78dc805e73cc1279af895edceac
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-08 12:27:18 +0500

    database connected

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://ridsashabbir:ridsashabbir123@gmail.com@booknook.kupodxs.mongodb.net/books-collection?retryWrites=true&w=majority";
