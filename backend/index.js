0f503fab27025ca33de8c9c37090090182c63b8f
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-09 12:14:11 +0500

    created the react project

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use(cors());
 
 // option 2: allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
