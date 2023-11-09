507501f35b3c764817cb12ab0c3c978430f7f7f1
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-09 13:36:46 +0500

    implemented react routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 // import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import CreateBook from "./Components/CreateBook";
+import EditBook from "./Components/EditBook";
+import DeleteBook from "./Components/DeleteBook";
+import ShowBook from "./Components/ShowBook";
 
 const App = () => {
   return (
     <Routes>
-      {/* <Route path="" element={} />
-      <Route path="" element={} />
-      <Route path="" element={} />
-      <Route path="" element={} />
-      <Route path="" element={} /> */}
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 };
