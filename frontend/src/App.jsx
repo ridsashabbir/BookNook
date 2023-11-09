6ce8de1b1da7e38733e8b5c4951c91492eb55d47
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-09 13:33:17 +0500

    react routers and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+// import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      {/* <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} /> */}
+    </Routes>
+  );
 };
 
 export default App;
