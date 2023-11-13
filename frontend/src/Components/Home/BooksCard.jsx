38ee18fb2f3f4ccf049d605ef23618779b7921ee
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-13 22:18:38 +0500

    render single card component

--- frontend/src/Components/Home/BooksCard.jsx
+++ frontend/src/Components/Home/BooksCard.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import "./BooksCard.css"; // Import your CSS file
+import BooksSingleCard from "./BooksSingleCard";
 
 const BooksCard = ({ books }) => {
   return (
     <div className="books-container">
       {books.map((item) => (
-        
+        <BooksSingleCard key={item._id} book={item} />
       ))}
     </div>
   );
