37eb1b4df9e6f5f74f1f103c96a114d249e9fe73
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-13 22:38:39 +0500

    added styling

--- frontend/src/Components/Home/BookModal.jsx
+++ frontend/src/Components/Home/BookModal.jsx
@@ -2,6 +2,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { BiUserCircle } from "react-icons/bi";
 import PropTypes from "prop-types";
+import "./BookModal.css";
 
 const BookModal = ({ book, onClose }) => {
   const handleModalClick = (event) => {
@@ -12,38 +13,25 @@ const BookModal = ({ book, onClose }) => {
   };
 
   return (
-    <div
-      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
-      onClick={handleModalClick}
-    >
+    <div className="fixed-overlay" onClick={handleModalClick}>
       <div
         onClick={(event) => event.stopPropagation()}
-        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative modal-content"
+        className="modal-content"
       >
-        <AiOutlineClose
-          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
-          onClick={onClose}
-        />
-        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
-          {book.publishYear}
-        </h2>
-        <h4 className="my-2 text-gray-500">{book._id}</h4>
-        <div className="flex justify-start items-center gap-x-2">
-          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+        <AiOutlineClose className="close-icon" onClick={onClose} />
+        <h2 className="publish-year-badge">{book.publishYear}</h2>
+        <h4 className="book-details">{book._id}</h4>
+        <div className="icon-text">
+          <PiBookOpenTextLight className="text-icon" />
           <h2 className="my-1">{book.title}</h2>
         </div>
-        <div className="flex justify-start items-center gap-x-2">
-          <BiUserCircle className="text-red-300 text-2xl" />
+        <div className="icon-text">
+          <BiUserCircle className="text-icon" />
           <h2 className="my-1">{book.author}</h2>
         </div>
-        <p className="mt-4">Anything You want to show</p>
-        <p className="my-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
-          voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
-          necessitatibus assumenda laboriosam, facilis iste cumque a pariatur
-          nesciunt cupiditate voluptas? Quis atque earum voluptate dolor nisi
-          dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
-          vitae voluptate sequi repellat!
+        <p className="mt-4 book-description">Anything You want to show</p>
+        <p className="my-2 book-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit...
         </p>
       </div>
     </div>
