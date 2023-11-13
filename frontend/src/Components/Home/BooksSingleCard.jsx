0502bc7226f2696172e97fdef19960e8b8f581a4
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-13 22:34:36 +0500

    render modal component

--- frontend/src/Components/Home/BooksSingleCard.jsx
+++ frontend/src/Components/Home/BooksSingleCard.jsx
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types";
+import { BiShow } from "react-icons/bi";
+import { useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { PiBookOpenTextLight } from "react-icons/pi";
+import BookModal from "./BookModal";
 
 const BooksSingleCard = ({ book }) => {
+  const [showModal, setShowModal] = useState(false);
+  console.log(showModal);
+
   return (
     <div key={book._id} className="book-card">
       <h2 className="publish-year">{book.publishYear}</h2>
@@ -20,6 +26,10 @@ const BooksSingleCard = ({ book }) => {
         <h2 className="author">{book.author}</h2>
       </div>
       <div className="icons">
+        <BiShow
+          className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+          onClick={() => setShowModal(true)}
+        />
         <Link to={`/books/details/${book._id}`} className="info">
           <BsInfoCircle />
         </Link>
@@ -30,6 +40,9 @@ const BooksSingleCard = ({ book }) => {
           <MdOutlineDelete />
         </Link>
       </div>
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 };
